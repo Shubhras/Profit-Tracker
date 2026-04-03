55074a592d586d450e953c6daaa47d9916bca7b4
Ghanshyam nagar <ghanshyam.nagar@digiprima.com>
2026-04-03 08:11:20 +0530

    amozone connect

---
+++ src/redux/amazonAPI/actionCreator.js
@@ -0,0 +1,26 @@
+import actions from './actions';
+import { DataService } from '../../config/dataService/dataService';
+
+const { amazonActionBegin, amazonActionSuccess, amazonActionErr } = actions;
+
+const amazonAction = (params, callback) => {
+  return async (dispatch) => {
+    dispatch(amazonActionBegin());
+    try {
+      const queryString = new URLSearchParams(params).toString();
+      console.log('aaaaaaaaaaaaaaaaa', queryString);
+      const response = await DataService.get(`/amazon/callback/?${queryString}`);
+      if (response.data.status === true) {
+        dispatch(amazonActionSuccess(response.data.data));
+        if (callback) callback(response.data.data);
+      } else {
+        dispatch(amazonActionErr(response.data.message || 'Something went wrong'));
+      }
+    } catch (err) {
+      console.error(err);
+      dispatch(amazonActionErr(err.response?.data?.message || err.message));
+    }
+  };
+};
+
+export { amazonAction };
